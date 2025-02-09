4a757ad6b000dedfbfa8d22879cab9840fa125f1
yeldesalim <157097573+yeldesalim@users.noreply.github.com>
2025-02-09 12:31:57 +0600

    Create 5.11.js

---
+++ lab3/Data types/5.11.js
@@ -0,0 +1,74 @@
+// tsk 1
+function getWeekDay(date) {
+    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+  
+    return days[date.getDay()];
+  }
+  
+  let date = new Date(2014, 0, 3); // 3 Jan 2014
+  alert( getWeekDay(date) ); // FR
+
+// task 2
+function getLocalDay(date) {
+
+    let day = date.getDay();
+  
+    if (day == 0) { // weekday 0 (sunday) is 7 in european
+      day = 7;
+    }
+  
+    return day;
+  }
+
+//  tsk 3
+function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
+  }
+  
+  alert( getLastDayOfMonth(2012, 0) ); // 31
+  alert( getLastDayOfMonth(2012, 1) ); // 29
+  alert( getLastDayOfMonth(2013, 1) ); // 28
+
+//   task 4
+function formatDate(date) {
+  let diff = new Date() - date; // the difference in milliseconds
+
+  if (diff < 1000) { // less than 1 second
+    return 'right now';
+  }
+
+  let sec = Math.floor(diff / 1000); // convert diff to seconds
+
+  if (sec < 60) {
+    return sec + ' sec. ago';
+  }
+
+  let min = Math.floor(diff / 60000); // convert diff to minutes
+  if (min < 60) {
+    return min + ' min. ago';
+  }
+
+  // format the date
+  // add leading zeroes to single-digit day/month/hours/minutes
+  let d = date;
+  d = [
+    '0' + d.getDate(),
+    '0' + (d.getMonth() + 1),
+    '' + d.getFullYear(),
+    '0' + d.getHours(),
+    '0' + d.getMinutes()
+  ].map(component => component.slice(-2)); // take last 2 digits of every component
+
+  // join the components into date
+  return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+}
+
+alert( formatDate(new Date(new Date - 1)) ); // "right now"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 sec. ago"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 min. ago"
+
+// yesterday's date like 31.12.2016 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
